fce82d2271b603be413a906a9a41f4a62eff48c9
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-19 18:23:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,6 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
  		{
@@ -38,7 +37,7 @@ def alan_kay_is_known_for
         }
      }
      
-     return programmer_hash[:alan_key][:known_for]
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
