23b45a079d3812df12ba5cac3088b3dc162c6f5f
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-19 18:58:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,10 +82,14 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"] }
+    programmer_hash[:yukihiro_matsumoto] = {
+      known_for: "Ruby", 
+      languages: ["LISP", "C"] 
+      
+    }
     
      return programmer_hash
-   binding.pry
+   
 end
 
 def changing_alan
@@ -106,7 +110,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash
      
 end
 
